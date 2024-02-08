5b9d8c0112d7f3c5dce5025e71bf55600b2e14c5
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2024-02-08 18:26:31 -0300

    Update usersRequire.php

--- Back-end/class/usersRequire.php
+++ Back-end/class/usersRequire.php
@@ -1,10 +1,10 @@
 <?php
-require_once ('/Back-end/class/conexao/Conexao.class.php');
-require_once ('/Back-end/class/users/Usuarios.class.php');
-require_once ('/Back-end/class/users/Instituicao.class.php');
-require_once ('/Back-end/class/users/CategoriaUsuario.class.php');
-require_once ('/Back-end/class/users/Pergunta.class.php');
-require_once ('/Back-end/class/users/RedeSocial.class.php');
+require_once "/Back-end/class/conexao/Conexao.class.php";
+require_once "/Back-end/class/users/Usuarios.class.php";
+require_once "/Back-end/class/users/Instituicao.class.php";
+require_once "/Back-end/class/users/CategoriaUsuario.class.php";
+require_once "/Back-end/class/users/Pergunta.class.php";
+require_once "/Back-end/class/users/RedeSocial.class.php";
 
 
 
